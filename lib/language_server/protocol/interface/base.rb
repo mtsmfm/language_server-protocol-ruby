68578d57905189f934cac822995246eeb0fffbb3
alpaca-tc <alpaca-tc@alpaca.tc>
2017-09-26 01:46:14 +0900

    Remove difficult to analyze methods
    Keywords arguments is easy to analyze

--- lib/language_server/protocol/interface/base.rb
+++ lib/language_server/protocol/interface/base.rb
@@ -10,57 +10,24 @@ module LanguageServer
           METHOD
         end
 
-        def self.required_keys
-          @required_keys ||= []
-        end
-
-        def self.optional_keys
-          @optional_keys ||= []
-        end
-
-        def self.attr_required_keys(*keys)
+        def self.define_attribute_methods(*keys)
           keys.each { |key| define_attribute_method(key) }
-          @required_keys = required_keys + keys
-        end
-
-        def self.attr_optional_keys(*keys)
-          keys.each { |key| define_attribute_method(key) }
-          @optional_keys = optional_keys + keys
         end
 
         attr_reader :attributes
 
         def initialize(attributes = {})
-          reject_extra_keys!(attributes.keys)
-
-          @attributes = (self.class.required_keys + self.class.optional_keys).each_with_object({}) { |key, h|
-            next unless attributes.key?(key)
+          @attributes = attributes.each_with_object({}) { |(key, value), h|
+            next if value.nil?
 
             # attributes[:text_document] => h[:textDocument]
             lower_camel_case = LanguageServer::Protocol::Utils.to_lower_camerize(key.to_s)
-            h[lower_camel_case.to_sym] = attributes[key]
+            h[lower_camel_case.to_sym] = value
           }.freeze
-
-          validate_required_keys!
         end
 
         def to_json(*args)
           attributes.to_json(*args)
-        end
-
-        private
-
-        def reject_extra_keys!(keys)
-          allowed_keys = self.class.required_keys + self.class.optional_keys
-          extra_keys = keys - allowed_keys
-
-          raise ArgumentError, "extra keys given: #{extra_keys}. allowed keys are #{allowed_keys}" unless extra_keys.empty?
-        end
-
-        def validate_required_keys!
-          required_keys = self.class.required_keys.map { |key| LanguageServer::Protocol::Utils.to_lower_camerize(key.to_s).to_sym }
-          missing_keys = required_keys - @attributes.keys
-          raise ArgumentError, "missing required key: #{missing_keys}" unless missing_keys.empty?
         end
       end
     end
--- test/language_server/protocol/interface/base_test.rb
+++ test/language_server/protocol/interface/base_test.rb
@@ -1,54 +1,16 @@
 require "test_helper"
 
 class LanguageServer::Protocol::Interface::BaseTest < Minitest::Test
-  def build_class(parent_class = LanguageServer::Protocol::Interface::Base)
-    Class.new(parent_class)
-  end
-
-  def test_that_it_defines_required_keys
-    inherited_interface = build_class
-    inherited_interface.attr_required_keys(:required)
-
-    assert_raises(ArgumentError) do
-      inherited_interface.new
-    end
-
-    # not raised
-    inherited_interface.new(required: 1)
-  end
-
-  def test_that_it_raises_argument_error_given_extra_keys
-    inherited_interface = build_class
-
-    assert_raises(ArgumentError) do
-      inherited_interface.new(extra: 1)
-    end
-  end
-
-  def test_that_it_defines_optional_keys
-    inherited_interface = build_class
-    inherited_interface.attr_optional_keys(:optional)
-
-    # not raised
-    inherited_interface.new(optional: 1)
+  def test_that_it_assigns_lower_camel_case_keys
+    instance = LanguageServer::Protocol::Interface::Base.new(underscore_case: 1)
+    assert(instance.attributes[:underscoreCase], 1)
   end
 
   def test_that_it_does_not_inherit_required_keys_from_parent_class
-    parent_class = build_class
-    parent_class.attr_required_keys(:required)
-    assert_equal(parent_class.required_keys, [:required])
+    inherited_klass = Class.new(LanguageServer::Protocol::Interface::Base)
+    inherited_klass.define_attribute_method(:key)
+    instance = inherited_klass.new(key: 1)
 
-    child_class = build_class(parent_class)
-    assert_empty(child_class.required_keys)
-  end
-
-  def test_that_it_defines_accessor_method
-    klass = build_class
-    klass.attr_required_keys(:required)
-    klass.attr_optional_keys(:optional)
-
-    instance = klass.new(required: 1, optional: 2)
-    assert(instance.required, 1)
-    assert(instance.optional, 2)
+    assert(instance.key, 1)
   end
 end
