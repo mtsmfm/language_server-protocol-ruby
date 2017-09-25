3937787278b1fab39ceceba7129926e11e4da5bf
alpaca-tc <alpaca-tc@alpaca.tc>
2017-09-26 01:48:28 +0900

    Message has jsonrpc version

--- lib/language_server/protocol/interface/message.rb
+++ lib/language_server/protocol/interface/message.rb
@@ -2,9 +2,11 @@ module LanguageServer
   module Protocol
     module Interface
       class Message < Base
+        VERSION = '2.0'
+
         define_attribute_methods(:jsonrpc)
 
-        def initialize(jsonrpc:)
+        def initialize(jsonrpc: VERSION)
           super
         end
       end
